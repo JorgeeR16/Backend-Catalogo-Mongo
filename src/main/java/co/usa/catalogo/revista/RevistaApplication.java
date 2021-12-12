570b9046a49db3fba4b39b80b11b4e5e4a41cf81
jorgeramirez2mt <91513363+jorgeramirez2mt@users.noreply.github.com>
2021-12-12 16:35:49 -0500

    modificando el ppal para que borre las documentos v2

--- src/main/java/co/usa/catalogo/revista/RevistaApplication.java
+++ src/main/java/co/usa/catalogo/revista/RevistaApplication.java
@@ -4,9 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
+
 import co.usa.catalogo.revista.Interfaz.CookwareCrudrepositorio;
 import co.usa.catalogo.revista.Interfaz.UserCrudrepositorio;
 
+
+@Component
 @SpringBootApplication
 public class RevistaApplication implements CommandLineRunner {
 
