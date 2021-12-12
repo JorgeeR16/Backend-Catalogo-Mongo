e543cb8c1f4ffec00ed691d1f03b8cb47ad7e14d
jorgeramirez2mt <91513363+jorgeramirez2mt@users.noreply.github.com>
2021-12-12 16:33:08 -0500

    modificando el ppal para que borre las documentos

--- src/main/java/co/usa/catalogo/revista/RevistaApplication.java
+++ src/main/java/co/usa/catalogo/revista/RevistaApplication.java
@@ -1,13 +1,29 @@
 package co.usa.catalogo.revista;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import co.usa.catalogo.revista.Interfaz.CookwareCrudrepositorio;
+import co.usa.catalogo.revista.Interfaz.UserCrudrepositorio;
 
 @SpringBootApplication
-public class RevistaApplication {
+public class RevistaApplication implements CommandLineRunner {
+
+
+	@Autowired 
+	private UserCrudrepositorio userCrudrepositorio;
+
+	@Autowired 
+	private CookwareCrudrepositorio cookwareCrudRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(RevistaApplication.class, args);
+	}
+	@Override
+	public void run (String... args) throws Exception{
+		cookwareCrudRepository.deleteAll();
+		userCrudrepositorio.deleteAll();
 	}
 
 }
